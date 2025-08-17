67222769b7c99fd8e1bab71f7e2e5e018c1b837f
Dhananjay <dhananjay.latkar@gmail.com>
2025-08-17 19:54:26 +0530

    11 Uniforms in OpenGL

--- src/Application.cpp
+++ src/Application.cpp
@@ -120,6 +120,9 @@ int main(void) {
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  /* make fps same as monitor's fps */
+  glfwSwapInterval(1);
+
   int err = glewInit();
   // if (err != GLEW_OK) {
   //     std::cout << "glewInit failed " << err << std::endl;
@@ -166,13 +169,27 @@ int main(void) {
       CreateShader(source.VertexSource, source.FragmentSource);
   GLCall(glUseProgram(shader));
 
+  int location = glGetUniformLocation(shader, "u_Color");
+  ASSERT(location != -1);
+
+  float r = 0.0f;
+  float increment = 0.05f;
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
     // draw the elements
+    GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
     GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+    if (r > 1.0f)
+      increment = -0.05f;
+    else if (r < 0.0f)
+      increment = 0.05f;
+
+    r += increment;
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
